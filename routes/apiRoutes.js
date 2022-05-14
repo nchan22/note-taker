166159f38d292e22e8bad0a065c53b022783b6fa
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-05-14 15:50:59 -0400

    Added function to get a single note by id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,9 +7,29 @@ const fs = require("fs");
 // create a function that GETS all notes by reading the db.json file
 // and returns the data
 router.get("/notes", function (req, res) {
-    fs.readFile("./db/db.json", (err, data) => {
-        if (err) throw err;
-        notes = JSON.parse(data);
-        return res.json(notes);
-    });
+  fs.readFile("./data/db.json", (err, data) => {
+    if (err) throw err;
+    notes = JSON.parse(data);
+    return res.json(notes);
+  });
+});
+// create a GET function that gets notes by ID
+router.get("/notes/:id", function (req, res) {
+  var id = req.params.id;
+  fs.readFileSync("./data/db.json", (err, data) => {
+    if (err) throw err;
+    notes = JSON.parse(data);
+  });
+
+  console.log("Looking for note ID: " + id + "=============");
+
+  for (var i = 0; i < notes.length; i++) {
+    if (notes[i].id == id) {
+      console.log("Found:");
+      console.log(notes[i]);
+      return res.json(notes[i]);
+    }
+  }
+
+  res.json(id);
 });
