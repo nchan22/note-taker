3a12b0a6db4261fb08d8c15c9d34ceee9e8d8e3c
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-05-14 19:55:56 -0400

    Updated issue with starting server
    Created separate server.js file and installed express.js npm

---
+++ server.js
@@ -0,0 +1,24 @@
+// Dependencies=====================================
+var express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
+// Sets up the Express App
+// =================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+// Lets express know to interpret incoming data as a json object
+app.use(express.json());
+app.use(express.static(__dirname + "/"));
+
+//routes
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+// Starts the server to begin listening=========================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
