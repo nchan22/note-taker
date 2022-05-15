dc84c91ee910e03b17a25421034e22fb22153bed
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-05-15 14:25:52 -0400

    Undid change

--- server.js
+++ server.js
@@ -1,59 +1,24 @@
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-const { get } = require("express/lib/response");
+// Dependencies=====================================
+var express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
-const port = process.env.PORT || 3001;
+// Sets up the Express App
+// =================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-const app = express();
-
-const notes = [];
-app.use(express.json());
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
+// Lets express know to interpret incoming data as a json object
+app.use(express.json());
+app.use(express.static(__dirname + "/"));
 
-app.use(express.static("public"));
+//routes
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.js"));
+// Starts the server to begin listening=========================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
 });
-
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-
-app.get("/api/notes", (req, res) => {
-  let file = fs.readFileSync("db/db.json");
-  let content = JSON.parse(file);
-  res.json(content);
-});
-
-app.post("/api/notes", (req, res) => {
-  const file = fs.readFileSync("db/db.json");
-  const content = JSON.parse(file);
-  console.log(content);
-  const note = { ...req.body, id: content.length + 1 };
-
-  content.push(note);
-  fs.writeFileSync("db/db.json", JSON.stringify(content));
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-
-app.delete("/api/notes/:id", (req, res) => {
-  let fileContent = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-  let noteID = req.params.id;
-  let index = 0;
-  fileContent = fileContent.filter((note) => {
-    console.log("line 47:", note);
-    return note.id != noteID;
-  });
-  for (not of fileContent) {
-    not.id = index.toString();
-    index++;
-  }
-  res.json(fileContent);
-  fs.writeFileSync("./db/db.json", JSON.stringify(fileContent));
-});
-
-app.listen(port, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
